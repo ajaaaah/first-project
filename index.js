6e4be6ca2325b1ba89de3b7989ee0a41350175b3
ajaaaah <ajah14@ymail.com>
2025-03-14 14:27:41 -0400

    Add smile button to both html and js file.

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ alert("Git it.");
 function gitIt() {
     console.log("Git it.");
 }
+
+function smiley(){
+    message = document.getElementById('smilebutton').innerHTML = "Thank you! 😊";
+    return message; 
+}
