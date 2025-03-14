3b51f1e476371ec981db8c9a2ee5e0c7bd83be62
ajaaaah <ajah14@ymail.com>
2025-03-14 15:55:35 -0400

    Add home.html.

--- index.js
+++
@@ -1,9 +0,0 @@
-alert("Git it.");
-function gitIt() {
-    console.log("Git it.");
-}
-
-function smiley(){
-    message = document.getElementById('smilebutton').innerHTML = "Thank you! 😊";
-    return message; 
-}
